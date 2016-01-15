a31dc821f48f54585d6b1831f459117bce463f8a
elionaz <jonathan@lunave.com>
2016-01-15 15:07:58 -0600

    update sql

---
+++ sql/frenchs.sql
@@ -0,0 +1,19 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50542
+ Source Host           : localhost
+ Source Database       : frenchs
+
+ Target Server Type    : MySQL
+ Target Server Version : 50542
+ File Encoding         : utf-8
+
+ Date: 01/15/2016 15:07:47 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
