85ac38b16c88289f801f99d483dca3f1547779d1
Jesus Antonio Garcia <jesusgarcia@iMac-Jesus.local>
2016-01-18 19:08:33 -0600

    Creation of partials files

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+        <header>
+            <div class="head">
+                <div class="hamburguesa"></div>
+                <div class="btn-close-menu"></div>
+                <a href="index.php"><h1 class="logo"><span>French´s</span></h1></a>
+                                
+                <nav class="menu">
+                    <ul>
+                        <li><a href="acerca.php">SOBRE NOSOTROS</a></li>
+                        <li><a href="productos.php">PRODUCTOS</a></li>
+                        <li><a href="recetas.php">RECETAS</a></li>
+                        <li><a href="contacto.php">CONTACTO</a></li>
+                    </ul>
+                </nav>
+
+                <div class="redes">
+                    <ul>
+                        <li><a target="_blank" href="" class="fa fa-instagram"></a></li>
+                        <li><a target="_blank" href="" class="fa fa-pinterest"></a></li>
+                        <li><a target="_blank" href="" class="fa fa-facebook"></a></li>
+                    </ul>
+                </div>
+                
+            </div>
+        </header>
