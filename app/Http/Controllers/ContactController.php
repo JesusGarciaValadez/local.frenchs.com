47d6e17e22640696e02687674f3f1c83f6960f65
Jesús Antonio García Valadez <jesus.garcia@lunave.com>
2016-01-19 23:20:03 +0000

    New method for process contact form and send email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,8 +9,62 @@ use App\Http\Controllers\Controller;
 
 class ContactController extends Controller
 {
+    protected $_subject = '';
+
     public function index ()
    	{
    		return view( 'contacto' );
    	}
+
+    public function send ( Request $request )
+    {
+      // Retrieving of all input data from contact form
+      $data = $request->all();
+
+      $this->_subject = 'Formulario de contacto';
+
+      /*
+       * Setting validation rules
+       */
+      $validator = \Validator::make( $data, [
+        'name'      => 'required|max:255',
+        'email'     => 'required|max:255',
+        'comments'  => 'required|max:255',
+      ] );
+
+      if ( $validator->fails() )
+      {
+        /*
+         * If validation fails, send response via JSON with an error code
+         */
+        // return redirect( 'home' )
+        //     ->withInput()
+        //     ->withErrors( $validator );
+        return response()->json( [ 'response_message' => 'error', 'response_code' => '0' ] );
+      }
+      else
+      {
+        /*
+         * Sending the email
+         */
+        \Mail::send( 'emails.message', $data, function( $message ) use ( $request )
+        {
+          // Setting sender
+          $message->from( $request->email, $request->name );
+
+          // Setting subject
+          $message->subject( $this->_subject );
+
+          // Setting receiver
+          $message->to( env( 'CONTACT_MAIL' ), env( 'CONTACT_NAME' ) );
+        } );
+
+        /*
+         * Response via JSON with a success code
+         */
+        return response()->json( [ 'response_message' => 'success', 'response_code' => '1' ] );
+      }
+
+    }
+
 }
