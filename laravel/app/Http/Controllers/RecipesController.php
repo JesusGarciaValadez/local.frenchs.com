d79fe2cb1c076d4cfa44e2eb24a88a64e893cdd5
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-22 20:56:04 +0000

    Data validation. ToDo: Check validation of recipe ingredients field

--- laravel/app/Http/Controllers/RecipesController.php
+++ laravel/app/Http/Controllers/RecipesController.php
@@ -27,23 +27,35 @@ class RecipesController extends Controller
     {
       // Retrieving of all input data from contact form
       $data = $request->all();
+      unset( $data[ '_token' ] );
+      $data[ 'recipe_ingredients_desktop' ] = $data[ 'recipe_ingredients' ];
+      $data[ 'recipe_ingredients_mobile' ]  = $data[ 'recipe_ingredients' ];
 
+      unset( $data[ 'recipe_ingredients' ] );
+
+      // Setting the subject for the email sended to alert about a new recipe is sended
       $this->_subject = 'Han enviado una nueva receta.';
 
       /*
        * Setting validation rules
        */
       $validator = \Validator::make( $data, [
-        'user_name'               => 'required|max:255|alpha',
-        'user_email'              => 'required|max:255|email|alpha_dash',
-        'recipe_name'             => 'required|max:255|alpha',
-        'recipe_photo'            => 'required|mimes:png,jpeg',
-        'recipe_categories'       => 'required|max:255|alpha|exists:recipes_categories,name',
-        'recipe_portions'         => 'required|max:255|alpha_num|in:1,2,3,4,5,6',
-        'recipe_preparation_time' => 'required|max:255|alpha_num|in:5 minutos,10 minutos,15 minutos,20 minutos,25 minutos,30 minutos',
-        'recipe_cooking_time'     => 'required|max:255|alpha_num|in:5 minutos,10 minutos,15 minutos,20 minutos,25 minutos,30 minutos',
-        'recipe_ingredients'      => 'required|max:255|alpha_num',
-        'recipe_preparation'      => 'required|max:255|alpha_num'
+        'user_name'                   => 'required|max:255|alpha',
+        'user_email'                  => 'required|max:255|email',
+        'recipe_name'                 => 'required|max:255|alpha',
+        'recipe_photo'                => 'required|mimes:png,jpeg',
+        'recipe_categorie'            => 'required|exists:recipes_categories,id',
+        'recipe_portions'             => 'required|in:1,2,3,4,5,6',
+        'recipe_preparation_time'     => 'required|in:5 min.,10 mins.,15 mins.,20 mins.,25 mins.,30 mins.',
+        'recipe_cooking_time'         => 'required|in:5 min.,10 mins.,15 mins.,20 mins.,25 mins.,30 mins.',
+        'recipe_ingredients_desktop'  => 'required|max:255|alpha_num',
+        'recipe_ingredients_mobile'   => 'required|max:255|same:recipe_ingredients_desktop',
+        'recipe_preparation'          => 'required|max:255'
+      ], [
+        'same'    => 'The :attribute and :other must match.',
+        'size'    => 'The :attribute must be exactly :size.',
+        'between' => 'The :attribute must be between :min - :max.',
+        'in'      => 'The :attribute must be one of the following types: :values',
       ] );
 
       if ( $validator->fails() )
@@ -51,15 +63,17 @@ class RecipesController extends Controller
         /*
          * If validation fails, send response via JSON with an error code
          */
-        // return redirect( 'home' )
-        //     ->withInput()
-        //     ->withErrors( $validator );
-        return response()->json( [ 'response_message' => 'Validation fail', 'response_code' => '0' ] );
+        //return response()->json
+        dd( [ 'response_message' => 'Validation fail', 'response_code' => '0', 'errors' => $validator->errors()->all(), 'Data: ' => $data ] );
       }
       else
       {
+        // Persist the data into the database. Checking if there's an existing recipe with this information.
+        // If not, stores the new recipe.
+        Recipes::firstOrCreate( $data );
+
         /*
-         * Sending the email
+         * Sending the email alerting about a new recipe.
          */
         \Mail::send( 'emails.message', $data, function( $message ) use ( $request )
         {
@@ -74,7 +88,7 @@ class RecipesController extends Controller
         } );
 
         /*
-         * Response via JSON with a success code
+         * Response via JSON with a success code.
          */
         return response()->json( [ 'response_message' => 'Success', 'response_code' => '1' ] );
       }
