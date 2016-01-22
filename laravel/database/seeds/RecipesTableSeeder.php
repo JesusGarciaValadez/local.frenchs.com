9c8c255eea12073969a1f199b314ef77afed3eda
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-22 18:48:52 +0000

    Add a seeder field for mobile version of the ingredients of each recipe. Change to enum in preparation time and portions fields of each recipe.

--- laravel/database/seeds/RecipesTableSeeder.php
+++ laravel/database/seeds/RecipesTableSeeder.php
@@ -6,30 +6,32 @@ use Faker\Factory as Faker;
 
 class RecipesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $faker = Faker::create();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
 
-        for( $i = 0; $i <= 19; $i++ )
-        {
-          DB::table( 'recipes' )->insert( [
-            'user_name'               => $faker->name,
-            'user_email'              => $faker->unique()->email,
-            'recipe_name'             => $faker->name,
-            'recipe_photo'            => 'receta-detalle.jpg',
-            'recipes_categories_id'   => $faker->numberBetween( 1, 10 ),
-            'recipe_portions'         => $faker->words( 1, true ),
-            'recipe_preparation_time' => $faker->words( 1, true ),
-            'recipe_cooking_time'     => $faker->words( 1, true ),
-            'recipe_ingredients'      => $faker->sentences( 1, true ),
-            'recipe_preparation'      => $faker->text,
-            'recipe_qualification'    => $faker->numberBetween( 1, 5 )
-          ] );
-        }
+    $faker = Faker::create();
+
+    for( $i = 0; $i <= 19; $i++ )
+    {
+      DB::table( 'recipes' )->insert( [
+        'user_name'                   => $faker->name,
+        'user_email'                  => $faker->unique()->email,
+        'recipe_name'                 => $faker->name,
+        'recipe_photo'                => 'receta-detalle.jpg',
+        'recipes_categories_id'       => $faker->numberBetween( 1, 10 ),
+        'recipe_portions'             => $faker->numberBetween( 1, 6 ),
+        'recipe_preparation_time'     => $faker->randomElement( [ '5 min.','10 mins.','15 mins.','20 mins.','25 mins.','30 mins.' ] ),
+        'recipe_cooking_time'         => $faker->randomElement( [ '5 min.','10 mins.','15 mins.','20 mins.','25 mins.','30 mins.' ] ),
+        'recipe_ingredients_desktop'  => $faker->sentences( 1, true ),
+        'recipe_ingredients_mobile'   => $faker->sentences( 1, true ),
+        'recipe_preparation'          => $faker->text,
+        'recipe_ranking'              => $faker->numberBetween( 1, 5 )
+      ] );
     }
+  }
 }
