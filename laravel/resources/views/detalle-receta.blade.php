5600ee20bd4355ccc6e9d97567057384258fb329
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-22 19:44:57 +0000

    Replacing hard coded recipe info with database recipes

--- laravel/resources/views/detalle-receta.blade.php
+++ laravel/resources/views/detalle-receta.blade.php
@@ -1,6 +1,6 @@
 @extends( 'layouts.layout' )
 
-@section( 'title', "Receta" )
+@section( 'title', 'Receta para preparar ' . $recipe->recipe_name )
 
 @section( 'content' )
         <div class="cover-interior-c"></div>
@@ -8,33 +8,33 @@
             <div class="header">
                 <div class="container">
                     <div class="info left"></span>
-                            <h3>Salmón <span>con mostaza y miel</span></h3>
-                            <div class="ranking">
-                                <span class="stars s4"></span>
+                        <h3>{{$recipe->recipe_name}}</h3>
+                        <div class="ranking">
+                            <span class="stars s{{$recipe->recipe_ranking}}"></span>
+                        </div>
+                        <div class="share">
+                            Comparte esta receta:<a href="#" class="btn-share"></a>
+                            <div class="redes">
+                                <ul>
+                                    <li><a target="_blank" href="" class="fa fa-pinterest"></a></li>
+                                    <li><a target="_blank" href="" class="fa fa-facebook"></a></li>
+                                </ul>
                             </div>
-                            <div class="share">
-                                Comparte esta receta:<a href="#" class="btn-share"></a>
-                                <div class="redes">
-                                    <ul>
-                                        <li><a target="_blank" href="" class="fa fa-pinterest"></a></li>
-                                        <li><a target="_blank" href="" class="fa fa-facebook"></a></li>
-                                    </ul>
-                                </div>
+                        </div>
+                        <div class="detalles">
+                            <div class="porciones">
+                                <div class="left">Porciones:</div><div class="right">{{$recipe->recipe_portions}}</div>
                             </div>
-                            <div class="detalles">
-                                <div class="porciones">
-                                    <div class="left">Porciones:</div><div class="right">4</div>
-                                </div>
-                                <div class="tiempo-preparacion">
-                                    <div class="left">Tiempo de preparación:</div><div class="right">15 min</div>
-                                </div>
-                                <div class="tiempo-coccion">
-                                    <div class="left">Tiempo de cocción:</div><div class="right">20 min</div>
-                                </div>
+                            <div class="tiempo-preparacion">
+                                <div class="left">Tiempo de preparación:</div><div class="right">{{$recipe->recipe_preparation_time}}</div>
                             </div>
+                            <div class="tiempo-coccion">
+                                <div class="left">Tiempo de cocción:</div><div class="right">{{$recipe->recipe_cooking_time}}</div>
+                            </div>
+                        </div>
                     </div>
                     <div class="image right"></span>
-                        {!! Html::image( 'assets/images/recetas/receta-detalle.jpg', 'Salmón con mostaza con miel' ) !!}
+                        {!! Html::image( 'assets/images/recetas/' . $recipe->recipe_photo, $recipe->recipe_name ) !!}
                     </div>
                 </div>
             </div>
@@ -44,33 +44,12 @@
                         <div class="ingredientes left">
                             <div class="icono-ingredientes"></div>
                             <h3>Ingredientes</h3>
-                            <ul>
-                                <li>4 filetes de salmón de 4oz o 113g sin piel</li>
-                                <li>2 cucharadas de aceite de oliva</li>
-                                <li>3 cucharadas de mostaza clásica <span class="marca">SQ FRENCH’S ®</span></li>
-                                <li>1 1/2 cucharada de miel</li>
-                            </ul>
+                            {{$recipe->recipe_ingredients_desktop}}
                         </div>
                         <div class="preparacion right">
                             <div class="icono-preparacion"></div>
                             <h3>Preparación</h3>
-                            <p>
-                                <span class="paso">Precalienta</span>
-                                una sarten a fuego medio y unta el aceite de oliva
-                            </p>
-                            <p>
-                                <span class="paso">Cocina</span>
-                                la piel del salmón hacia el sarten por
-                                5 minutos
-                            </p>
-                            <p>
-                                <span class="paso">Mezcla</span>
-                                en un recipiente las 3 cucharada de mostaza clàsica <span class="marca">SQ FRENCH’S ®</span> con la 1 1/2 cucharada de miel unta la mezcla de mostaza y miel en el salmón y volteal, cocina durante 5 minutos
-                            </p>
-                            <p>
-                                <span class="paso">Sirve</span>
-                                con mostaza y miel adicional.
-                            </p>
+                            {{$recipe->recipe_preparation}}
                         </div>
                     </div>
 
@@ -86,12 +65,7 @@
                                 <div class="ingredientes">
                                     <div class="icono-ingredientes"></div>
                                     <h3>Ingredientes</h3>
-                                    <ul>
-                                        <li><span class="medida">4</span> filetes de salmón de 4oz o 113g sin piel</li>
-                                        <li><span class="medida">2</span> cucharadas de aceite de oliva</li>
-                                        <li><span class="medida">3</span> cucharadas de mostaza clásica <span class="marca">SQ FRENCH’S ®</span></li>
-                                        <li><span class="medida">1 1/2</span> cucharada de miel</li>
-                                    </ul>
+                                    {{$recipe->recipe_ingredients_mobile}}
                                 </div>
                             </div>
 
@@ -100,23 +74,7 @@
                                 <div class="preparacion">
                                     <div class="icono-preparacion"></div>
                                     <h3>Preparación</h3>
-                                    <p>
-                                        <span class="paso">Precalienta</span>
-                                        una sarten a fuego medio y unta el aceite de oliva
-                                    </p>
-                                    <p>
-                                        <span class="paso">Cocina</span>
-                                        la piel del salmón hacia el sarten por
-                                        5 minutos
-                                    </p>
-                                    <p>
-                                        <span class="paso">Mezcla</span>
-                                        en un recipiente las 3 cucharada de mostaza clàsica <span class="marca">SQ FRENCH’S ®</span> con la 1 1/2 cucharada de miel unta la mezcla de mostaza y miel en el salmón y volteal, cocina durante 5 minutos
-                                    </p>
-                                    <p>
-                                        <span class="paso">Sirve</span>
-                                        con mostaza y miel adicional.
-                                    </p>
+                                    {{$recipe->recipe_preparation}}
                                 </div>
 
                             </div>
@@ -132,42 +90,26 @@
                 <h2>Otras recetas</h2>
                 <p>que te interesaría preparar</p>
                 <div class="content-grid">
-                    <a href="" class="receta">
-                        <p class="categoria b2">Platillos</p>
+                    @foreach( $recipes as $recipe )
+                    <a href="{{ action( 'RecipeController@index', [ 'id' => $recipe->id ] ) }}" class="receta">
+                        <p class="categoria b2">
+                            @foreach ( $categories as $categorie )
+                                @if ( $categorie->id == $recipe->recipes_categories_id )
+                            {{$categorie->categorie_name}}
+                                @endif
+                            @endforeach
+                        </p>
                         <div class="image">
-                            {!! Html::image( 'assets/images/recetas/receta2.jpg', 'Tiras de pollo con mostaza' ) !!}
+                            {!! Html::image( 'assets/images/recetas/' . $recipe->recipe_photo, $recipe->recipe_name ) !!}
                         </div>
-                        <p class="nombre">Tiras de pollo con mostaza</p>
-                        <p class="porciones">12 porciones</p>
-                        <p class="tiempo">Timepo de preparación: 15 min.</p>
+                        <p class="nombre">{{$recipe->recipe_name}}</p>
+                        <p class="porciones">{{$recipe->recipe_portions}} porciones</p>
+                        <p class="tiempo">Tiempo de preparación: {{$recipe->recipe_preparation_time}}</p>
                         <div class="ranking">
-                            <span class="stars s4"></span>
+                            <span class="stars s{{$recipe->recipe_ranking}}"></span>
                         </div>
                     </a>
-                    <a href="" class="receta">
-                        <p class="categoria b2">Platillos</p>
-                        <div class="image">
-                            {!! Html::image( 'assets/images/recetas/receta2.jpg', 'Tiras de pollo con mostaza' ) !!}
-                        </div>
-                        <p class="nombre">Tiras de pollo con mostaza</p>
-                        <p class="porciones">12 porciones</p>
-                        <p class="tiempo">Timepo de preparación: 15 min.</p>
-                        <div class="ranking">
-                            <span class="stars s4"></span>
-                        </div>
-                    </a>
-                    <a href="" class="receta">
-                        <p class="categoria b2">Platillos</p>
-                        <div class="image">
-                            {!! Html::image( 'assets/images/recetas/receta2.jpg', 'Tiras de pollo con mostaza' ) !!}
-                        </div>
-                        <p class="nombre">Tiras de pollo con mostaza</p>
-                        <p class="porciones">12 porciones</p>
-                        <p class="tiempo">Timepo de preparación: 15 min.</p>
-                        <div class="ranking">
-                            <span class="stars s4"></span>
-                        </div>
-                    </a>
+                    @endforeach
                 </div>
                 <a id="" href="#" class="btn-mas">Ver más recetas</a>
             </div>
