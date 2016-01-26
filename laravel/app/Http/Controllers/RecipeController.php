823f7fb0f983bee234c9607043f5a9f44956d942
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-26 18:17:25 +0000

    Adding a method for update a recipe information.

--- laravel/app/Http/Controllers/RecipeController.php
+++ laravel/app/Http/Controllers/RecipeController.php
@@ -12,12 +12,19 @@ use frenchs\Http\Controllers\Controller;
 
 class RecipeController extends Controller
 {
-    public function index ( Request $request, Recipes $recipesSet, RecipesCategories $recipesCategories )
-    {
-      $recipe     = $recipesSet->findOrFail( $request->id );
-      $recipes    = $recipesSet->orderBy( 'created_at' )->take( 4 )->get();
-      $categories = $recipesCategories->all();
+  public function index ( Request $request, Recipes $recipesSet, RecipesCategories $recipesCategories )
+  {
+    $recipe     = $recipesSet->findOrFail( $request->id );
+    $recipes    = $recipesSet->orderBy( 'created_at' )->take( 4 )->get();
+    $categories = $recipesCategories->all();
 
-      return view( 'detalle-receta', [ 'recipe' => $recipe, 'recipes' => $recipes, 'categories' => $categories ] );
-    }
+    return view( 'detalle-receta', [ 'recipe' => $recipe, 'recipes' => $recipes, 'categories' => $categories ] );
+  }
+
+  public function update ( Request $request )
+  {
+    $recipe = $request;
+
+    dd( \Route );
+  }
 }
