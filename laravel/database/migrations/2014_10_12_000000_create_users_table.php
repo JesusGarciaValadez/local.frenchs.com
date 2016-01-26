0a949fab693133fa3b3135c23e4150d4c0d809b0
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-26 21:21:28 +0000

    Rollback of name of create editors table to create users table

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEditorsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateEditorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('editors', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
@@ -29,6 +29,6 @@ class CreateEditorsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('editors');
+        Schema::drop('users');
     }
 }
