24829d51c6b9b6b2ba393a74d7e4f455e57ebf53
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-26 17:30:21 +0000

    Contact email markup for send info from contact form.

---
+++ laravel/resources/views/emails/contact.blade.php
@@ -0,0 +1,3 @@
+<p><strong>Nombre:</strong> {{ $contact[ 'name' ] }}</p>
+<p><strong>Correo:</strong> {{ $contact[ 'email' ] }}</p>
+<p><strong>Mensaje:</strong> {{ $contact[ 'comments' ] }}</p>
--- laravel/resources/views/emails/message.blade.php
+++
@@ -1,4 +0,0 @@
-{{ $email }}
-{{ $comments }}
-{{-- $body --}}
-Gracias
