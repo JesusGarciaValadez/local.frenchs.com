4fbaec3ba3e3c038b0034338903684639e64d3b3
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-01-26 21:20:34 +0000

    Change name of the folder container for the products views

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -56,15 +56,15 @@ Route::group( [ 'prefix' => 'producto' ], function ()
 {
   // Mustards
   Route::group( [ 'prefix' => 'mostaza' ], function () {
-    Route::get( 'clasica-sq', [ 'as' => 'clasica-sq', function() { return view( 'producto.mostaza.clasica-sq' ); } ] );
-    Route::get( 'clasica-en-frasco', [ 'as' => 'clasica-frasco', function() { return view( 'producto.mostaza.clasica-en-frasco' ); } ] );
-    Route::get( 'dijon-sq', [ 'as' => 'dijon', function() { return view( 'producto.mostaza.dijon-sq' ); } ] );
-    Route::get( 'deli-sq', [ 'as' => 'deli', function() { return view( 'producto.mostaza.deli-sq' ); } ] );
-    Route::get( 'honey-sq', [ 'as' => 'honey', function() { return view( 'producto.mostaza.honey-sq' ); } ] );
+    Route::get( 'clasica-sq', [ 'as' => 'clasica-sq', function() { return view( 'products.mustard.clasica-sq' ); } ] );
+    Route::get( 'clasica-en-frasco', [ 'as' => 'clasica-frasco', function() { return view( 'products.mustard.clasica-en-frasco' ); } ] );
+    Route::get( 'dijon-sq', [ 'as' => 'dijon', function() { return view( 'products.mustard.dijon-sq' ); } ] );
+    Route::get( 'deli-sq', [ 'as' => 'deli', function() { return view( 'products.mustard.deli-sq' ); } ] );
+    Route::get( 'honey-sq', [ 'as' => 'honey', function() { return view( 'products.mustard.honey-sq' ); } ] );
   } );
 
   // Sauces
   Route::group( [ 'prefix' => 'salsa' ], function () {
-    Route::get( 'inglesa', [ 'as' => 'inglesa', function() { return view( 'producto.salsa.inglesa' ); } ] );
+    Route::get( 'inglesa', [ 'as' => 'inglesa', function() { return view( 'products.sauce.inglesa' ); } ] );
   } );
 } );
