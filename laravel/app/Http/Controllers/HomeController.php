46e4c2d7ff79eb1d9ebc620d660c04370102e9bf
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-04-01 19:47:18 +0000

    Setting for retrieve 3 random results with the query for Tinder like slider in home page

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 {
     public function index ( Recipes $recipesSet )
    	{
-      $recipes    = $recipesSet->where( 'active', true )->orderBy( 'created_at', 'desc' )->take( 3 )->get();
+      $recipes    = $recipesSet->where( 'active', true )->orderBy( 'created_at', 'desc' )->get()->random( 3 );
 
       return view( 'home', [ 'recipes' => $recipes ] );
    	}
