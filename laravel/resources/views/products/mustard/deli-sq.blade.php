c6e1a8450e388d89754ffa92c0169606db5f446d
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-05-06 15:38:16 +0000

    Fix nutritional info in mustard deli.

--- laravel/resources/views/products/mustard/deli-sq.blade.php
+++ laravel/resources/views/products/mustard/deli-sq.blade.php
@@ -17,18 +17,18 @@
                     </div>
                     <div class="ingredientes">
                         <h3>Ingredientes</h3>
-                        <p>Vinagre destilado, agua, semilla de mostaza grado 1, sal, especias, cúrcuma, saborizantes naturales,</p>
+                        <p>Vinagre, semilla de mostaza, agua, sal, especias, cúrcuma y saborizantes naturales. Contiene: mostaza.</p>
                     </div>
                     <div class="informacion">
                         <h3>Información Nutrimental</h3>
                         <div class="line"></div>
-                        <p>Cont. Neto: 12oz (340g)</p>
-                        <p>Porción: 1 cucharada (11.3g) Total: 30 Calorías: 340</p>
+                        <p>Cont. Neto: 340g</p>
+                        <p>Porción: 1 cucharada (30g) Calorías: 30 (125 kj)</p>
                         <table>
                             <thead>
                                 <tr>
-                                  <th colspan="2">Cantidades por porción.</th>
-                                  <th>0% DV*</th>
+                                  <th colspan="2"><strong>Cantidades por porción.</strong></th>
+                                  <th>DV*</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -44,11 +44,6 @@
                                 </tr>
                                 <tr>
                                   <td>Grasas Trans</td>
-                                  <td>0g</td>
-                                  <td>0%</td>
-                                </tr>
-                                <tr>
-                                  <td>Colesterol</td>
                                   <td>0g</td>
                                   <td>0%</td>
                                 </tr>
