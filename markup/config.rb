9bfc5ce1051585ea447a12f73383f201c2b08ae3
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-05-09 16:55:42 +0000

    Config.rb file configuration.

--- markup/config.rb
+++ markup/config.rb
@@ -3,13 +3,14 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "../laravel/public/assets/css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -23,3 +24,5 @@ javascripts_dir = "javascripts"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+sourcemap = (environment == :production) ? false : true
