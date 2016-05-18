2037d3d6a196aaddc69b76f5285f676ea5a7efce
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-05-18 22:54:12 +0000

    Use of DatabaseMigration trait in ProductsTest class

--- laravel/tests/ProductsTest.php
+++ laravel/tests/ProductsTest.php
@@ -6,38 +6,40 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ProductsTest extends TestCase
 {
-    /**
-     * Test products landing page availability.
-     *
-     * @return void
-     */
-    public function testProductsAvailability ()
-    {
-      $this->visit( '/' )
-           ->click( 'PRODUCTOS' )
-           ->assertResponseOk()
-           ->seePageIs( '/productos' )
-           ->see( "French's Classic Yellow - Mostaza Clásica" )
-           ->see( "French's Classic Yellow - Mostaza Clásica en frasco" )
-           ->see( "French's Mostaza Dijon" )
-           ->see( "French's Mostaza Deli" )
-           ->see( "Frenchs' Mostaza Honey - Sabor agridulce" )
-           ->see( "French's Worcestershire Sauce - Salsa inglesa" )
-           ->see( "Nueva French's BBQ Classic - Salsa BBQ Clásica" )
-           ->see( "Nueva French's BBQ Chipotle - Salsa BBQ sabor chipotle" );
-    }
+  use DatabaseMigrations;
 
-    /**
-     * Test Classic Mustard landing page avalability.
-     *
-     * @return void
-     */
-    public function testClassicMustardAvailability ()
-    {
-      $this->visit('/producto/mostaza/clasica-sq')
-           ->assertResponseOk()
-           ->see( "Mostaza Clásica" )
-           ->see( "Recetas" )
-           ->see( "que podrías preparar con Mostaza Clásica" );
-    }
+  /**
+   * Test products landing page availability.
+   *
+   * @return void
+   */
+  public function testProductsAvailability ()
+  {
+    $this->visit( '/' )
+         ->click( 'PRODUCTOS' )
+         ->assertResponseOk()
+         ->seePageIs( '/productos' )
+         ->see( "French's Classic Yellow - Mostaza Clásica" )
+         ->see( "French's Classic Yellow - Mostaza Clásica en frasco" )
+         ->see( "French's Mostaza Dijon" )
+         ->see( "French's Mostaza Deli" )
+         ->see( "Frenchs' Mostaza Honey - Sabor agridulce" )
+         ->see( "French's Worcestershire Sauce - Salsa inglesa" )
+         ->see( "Nueva French's BBQ Classic - Salsa BBQ Clásica" )
+         ->see( "Nueva French's BBQ Chipotle - Salsa BBQ sabor chipotle" );
+  }
+
+  /**
+   * Test Classic Mustard landing page avalability.
+   *
+   * @return void
+   */
+  public function testClassicMustardAvailability ()
+  {
+    $this->visit('/producto/mostaza/clasica-sq')
+         ->assertResponseOk()
+         ->see( "Mostaza Clásica" )
+         ->see( "Recetas" )
+         ->see( "que podrías preparar con Mostaza Clásica" );
+  }
 }
