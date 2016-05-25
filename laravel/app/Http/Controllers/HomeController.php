c0ad04cd1a0575c94a5db023ce00ba9933fd7ff1
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-05-25 17:15:54 +0000

    HomeController documentation

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -12,10 +12,18 @@ use frenchs\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function index ( Recipes $recipesSet )
-   	{
-      $recipes    = $recipesSet->where( 'active', true )->orderBy( 'created_at', 'desc' )->get()->random( 3 );
+  /**
+   * Return the home view with a bunch of recipes from the database
+   * @param  Recipes $recipesSet Set of recipes from the database
+   * @return View                View 'home' with a bunch of recipes as parameters
+   */
+  public function index ( Recipes $recipesSet )
+  {
+    $recipes    = $recipesSet->where( 'active', true )
+                             ->orderBy( 'created_at', 'desc' )
+                             ->get()
+                             ->random( 3 );
 
-      return view( 'home', [ 'recipes' => $recipes ] );
-   	}
+    return view( 'home', [ 'recipes' => $recipes ] );
+  }
 }
