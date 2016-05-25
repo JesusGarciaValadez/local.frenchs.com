b40d4e2798ce757aae8e5d1eac6d7ea3cca8cf52
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-05-25 22:20:39 +0000

    Modify table seeders for reflect changes in model names.

--- laravel/database/seeds/RecipesCategoriesTableSeeder.php
+++ laravel/database/seeds/RecipesCategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class RecipesCategoriesTableSeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -12,19 +12,19 @@ class RecipesCategoriesTableSeeder extends Seeder
     public function run()
     {
       // generate data by accessing properties
-      DB::table( 'recipes_categories' )->insert( [
-        'categorie_name'  => 'botana',
-        'created_at'      => date( 'Y-m-d H:i:s' ),
+      DB::table( 'categories' )->insert( [
+        'name'        => 'botana',
+        'created_at'  => date( 'Y-m-d H:i:s' ),
       ] );
 
-      DB::table( 'recipes_categories' )->insert( [
-        'categorie_name'  => 'entrada',
-        'created_at'      => date( 'Y-m-d H:i:s' ),
+      DB::table( 'categories' )->insert( [
+        'name'        => 'entrada',
+        'created_at'  => date( 'Y-m-d H:i:s' ),
       ] );
 
-      DB::table( 'recipes_categories' )->insert( [
-        'categorie_name'  => 'plato fuerte',
-        'created_at'      => date( 'Y-m-d H:i:s' ),
+      DB::table( 'categories' )->insert( [
+        'name'        => 'plato fuerte',
+        'created_at'  => date( 'Y-m-d H:i:s' ),
       ] );
     }
 }
