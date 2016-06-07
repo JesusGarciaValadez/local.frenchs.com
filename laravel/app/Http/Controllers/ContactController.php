1d29479d7b3ca89aa223cccfd9de1b384a7eb122
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-06-07 19:37:00 +0000

    Pass all validation structure in ContactController into a ContactFormRequest file for better readability.

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -4,7 +4,8 @@ namespace Frenchs\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Frenchs\Http\Requests;
+use App\Http\Requests;
+use Frenchs\Http\Requests\ContactFormRequest;
 use Frenchs\Http\Controllers\Controller;
 
 class ContactController extends Controller
@@ -29,32 +30,19 @@ class ContactController extends Controller
    * @param  Request $request Parameters of the request of the page.
    * @return String           JSON response with a message and code.
    */
-  public function send ( Request $request )
+  public function send ( ContactFormRequest $request )
   {
     $data = $request->all();
 
     $this->_subject = 'Formulario de contacto';
 
-    $validator = \Validator::make( $data, [
-      'name'      => 'required|max:255',
-      'email'     => 'required|email|max:255',
-      'comments'  => 'required|max:255',
-    ] );
-
-    if ( $validator->fails() )
+    \Mail::send( 'emails.contact', [ 'contact' => $data ], function( $message )
     {
-      return response()->json( [ 'response_message' => 'error', 'response_code' => '0' ] );
-    }
-    else
-    {
-      \Mail::send( 'emails.contact', [ 'contact' => $data ], function( $message )
-      {
-        $message->from( env( 'CONTACT_SENDER', 'forge' ), env( 'CONTACT_APP_NAME', 'forge' ) );
-        $message->subject( $this->_subject );
-        $message->to( env( 'CONTACT_MAIL', 'forge' ), env( 'CONTACT_NAME', 'forge' ) );
-      } );
+      $message->from( env( 'CONTACT_SENDER', 'forge' ), env( 'CONTACT_APP_NAME', 'forge' ) );
+      $message->subject( $this->_subject );
+      $message->to( env( 'CONTACT_MAIL', 'forge' ), env( 'CONTACT_NAME', 'forge' ) );
+    } );
 
-      return response()->json( [ 'response_message' => 'success', 'response_code' => '1' ] );
-    }
+    return response()->json( [ 'response_message' => 'success', 'response_code' => '1' ] );
   }
 }
